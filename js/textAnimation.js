cc1fa1d4872ef2a0c246d12df0e0b7d91ac22041
Zach Zwerling <44988490+zzwerling@users.noreply.github.com>
2020-02-29 16:54:17 -0500

    new setup

---
+++ js/textAnimation.js
@@ -0,0 +1,12 @@
+function printLetterByLetter(destination, message, speed){
+    var i = 0;
+    var interval = setInterval(function(){
+        document.getElementById(destination).innerHTML += message.charAt(i);
+        i++;
+        if (i > message.length){
+            clearInterval(interval);
+        }
+    }, speed);
+}
+
+printLetterByLetter("test1", "zachary.sh", 200);
